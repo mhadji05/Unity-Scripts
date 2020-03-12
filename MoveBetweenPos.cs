0ca19eaf3076438ac4065376ed318cc2cbd70e28
techjuego <61373753+techjuego@users.noreply.github.com>
2020-03-12 23:59:19 +0530

    Add files via upload

---
+++ MoveBetweenPos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetweenPos: MonoBehaviour {
+	public Transform FirstPos;
+	public Transform SecondPos;
+	float starttime;
+	public float movespeed;
+	public float TimeSpeed = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		starttime += Time.deltaTime * TimeSpeed;
+		transform.localPosition = Vector3.Lerp (FirstPos.position,SecondPos.position,(Mathf.Sin(starttime * movespeed + Mathf.PI/2)+1)/2);
+	
+	}
+}
